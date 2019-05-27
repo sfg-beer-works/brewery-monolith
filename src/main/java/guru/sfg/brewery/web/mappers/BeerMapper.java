4831dd06540ec93f08ed42cb08ea4f59ae926e14
John <john@springframework.guru>
2019-05-27 12:45:47 -0400

    adding beer mapper decorator

--- src/main/java/guru/sfg/brewery/web/mappers/BeerMapper.java
+++ src/main/java/guru/sfg/brewery/web/mappers/BeerMapper.java
@@ -19,9 +19,11 @@ package guru.sfg.brewery.web.mappers;
 
 import guru.sfg.brewery.domain.Beer;
 import guru.sfg.brewery.web.model.BeerDto;
+import org.mapstruct.DecoratedWith;
 import org.mapstruct.Mapper;
 
 @Mapper(uses = DateMapper.class)
+@DecoratedWith(BeerMapperDecorator.class)
 public interface BeerMapper {
 
     BeerDto beerToBeerDto(Beer beer);
---
+++ src/main/java/guru/sfg/brewery/web/mappers/BeerMapperDecorator.java
@@ -0,0 +1,33 @@
+package guru.sfg.brewery.web.mappers;
+
+import guru.sfg.brewery.domain.Beer;
+import guru.sfg.brewery.web.model.BeerDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+public abstract class BeerMapperDecorator implements BeerMapper{
+
+    private BeerMapper beerMapper;
+
+    @Autowired
+    @Qualifier("delegate")
+    public void setBeerMapper(BeerMapper beerMapper) {
+        this.beerMapper = beerMapper;
+    }
+
+    @Override
+    public BeerDto beerToBeerDto(Beer beer) {
+
+        BeerDto dto = beerMapper.beerToBeerDto(beer);
+
+        if(beer.getBeerInventory() != null && beer.getBeerInventory().size() > 0) {
+
+            dto.setQuantityOnHand(beer.getBeerInventory()
+                    .stream().map(beerInventory -> beerInventory.getQuantityOnHand())
+                    .reduce(0, Integer::sum));
+
+        }
+
+        return dto;
+    }
+}
